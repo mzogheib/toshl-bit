5d4f0c0d85318c3af0daf81f08d5533760cbcc80
Marwan Zogheib <mzogheib@gmail.com>
2019-02-21 20:22:22 +1100

    Add basic menu that sends messages to companion

--- app/index.js
+++ app/index.js
@@ -1,1 +1,26 @@
-console.log('Hello world!');
+import document from "document";
+import * as messaging from "messaging";
+
+messaging.peerSocket.onopen = function() {
+  console.log("App ready to send messages.");
+};
+
+messaging.peerSocket.onerror = function(err) {
+  console.log("Connection error: " + err.code + " - " + err.message);
+};
+
+function sendMessage(data) {
+  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
+    messaging.peerSocket.send(data);
+  }
+}
+
+let list = document.getElementById("my-list");
+let items = list.getElementsByClassName("tile-list-item");
+
+items.forEach((element, index) => {
+  let touch = element.getElementById("touch-me");
+  touch.onclick = evt => {
+    sendMessage(index);
+  };
+});
--- companion/index.js
+++ companion/index.js
@@ -1,1 +1,9 @@
-console.log('Hello world!');
+import * as messaging from "messaging";
+
+messaging.peerSocket.onopen = function() {
+  console.log("Companion ready to receive messages.");
+};
+
+messaging.peerSocket.onmessage = function(evt) {
+  console.log(JSON.stringify(evt.data));
+};
