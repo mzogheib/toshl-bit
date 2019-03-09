207ed0ea95919a13d91068601317a4a0f0108d60
Marwan Zogheib <mzogheib@gmail.com>
2019-03-09 15:41:13 +1100

    Add item highlighting on touch

--- app/screens/expenses/index.js
+++ app/screens/expenses/index.js
@@ -9,10 +9,17 @@ const onTouch = callback => (handleTouch = callback);
 
 items.forEach((item, index) => {
   const expense = expenses[index];
+  const background = item.getElementById("expenses__item-background");
+  const touch = item.getElementById("expenses__item-touch");
   item.getElementById("expenses__item-text-upper").text = expense.textUpper;
   item.getElementById("expenses__item-text-lower").text = expense.textLower;
-  item.getElementById("expenses__item-touch").onclick = () =>
-    handleTouch(expense.data);
+  touch.onclick = () => {
+    background.style.fill = "fb-slate-press";
+    setTimeout(() => {
+      handleTouch(expense.data);
+      background.style.fill = "fb-black";
+    }, 125);
+  };
 });
 
 export default {
